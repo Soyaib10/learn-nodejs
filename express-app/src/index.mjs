490ea67cd637db5d0d72ab6a84337c8d0b9ee859
Soyaib10 <soyaibzihad10@gmail.com>
2025-02-19 12:22:59 +0600

    implemented post method

--- express-app/src/index.mjs
+++ express-app/src/index.mjs
@@ -2,13 +2,16 @@
 import express from "express"
 
 // constant values
-const app = express();
+const app = express()
 const PORT = 3000
+
+app.use(express.json());
+
 const mockUsers = [
     { id: 1, username: "rrid", displayName: "rishan" },
     { id: 2, username: "soyaib", displayName: "zihad" },
     { id: 3, username: "amar valo lage na", displayName: "ha ha" },
-    { id: 4, username: "amar olpo valo lage na", displayName: "ha ha" },
+    { id: 4, username: "amar olpo valo lage na", displayName: "ha ha" }, 
     { id: 5, username: "amar besi valo lage na", displayName: "ha ha" },
     { id: 6, username: "amar valoi lage na", displayName: "ha ha" },
 ]
@@ -53,6 +56,18 @@ app.get("/api/users/:id", (req, res) => {
         return res.sendStatus(404)
     }
     return res.send(findUser)
+})
+
+//------------POST-----------------
+app.post("/api/users", (req, res) => {
+    console.log(req.body)
+    const {body} = req
+    const newUser = {
+        id: mockUsers.length + 1,
+        ...body
+    }
+    mockUsers.push(newUser)
+    return res.status(201).send(newUser)
 })
 
 // run the server
